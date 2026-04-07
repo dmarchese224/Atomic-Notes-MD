d4171f5abcc2cb653b90a8692d43075b2fce04da
dmarchese224 <doug.marchese@gmail.com>
2026-04-06 20:24:29 -0400

    Sidebar fix-delete

--- sw.js
+++
@@ -1,7 +0,0 @@
-const CACHE_NAME = 'atomic-notes-md-v1';
-const ASSETS = [
-  './', './index.html', './style.css', './app.js', './manifest.json',
-  './assets/favicon.ico', './assets/atomic-notes-md.svg', './assets/icon-180.png', './assets/icon-192.png', './assets/icon-512.png'
-];
-self.addEventListener('install', e => e.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS))));
-self.addEventListener('fetch', e => e.respondWith(caches.match(e.request).then(r => r || fetch(e.request))));
